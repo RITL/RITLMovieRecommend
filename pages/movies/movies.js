bcf318d6d2d123ab9fcc727f809c40dad010a021
774655048@qq.com <774655048@qq.com>
2017-11-16 21:29:35 +0800

    add request in movies.js

--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -1,2 +1,27 @@
 Page({
+    // RESTFul API JSON
+    // SOAP XML WSDL
+
+    onLoad: (event)=>{
+        wx.request({
+            url: 'https://api.douban.com/v2/movie/in_theaters',
+            header: {
+                'Content-Type' : 'application/xml'
+            },
+            data:{
+
+            },
+            success:(res)=>{
+                console.log(res);
+            },
+
+            fail:(error)=>{
+                console.log(error);
+            },
+
+            complete:()=>{
+
+            },
+        })
+    }
 })
