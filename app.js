b720799e2d19e7e56a9b4716f4907e98f9fe1b39
774655048@qq.com <774655048@qq.com>
2017-11-17 17:46:17 +0800

    add networking

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ App({
     //全局对象
     globalData: { 
         g_isPlayingMusic: false,
-        g_currentMusicPostId:null
+        g_currentMusicPostId:null,
+        doubanBase: 'https://api.douban.com',
     }
 })
          
--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -1,27 +1,46 @@
+var app = getApp();
+
 Page({
-    // RESTFul API JSON
+    // RESTFul API JSON ****需了解****
     // SOAP XML WSDL
 
-    onLoad: (event)=>{
-        wx.request({
-            url: 'https://api.douban.com/v2/movie/in_theaters',
-            header: {
-                'Content-Type' : 'application/xml'
-            },
-            data:{
+    onLoad: function(option) {
 
+        // 基础的url
+        var baseUrl = app.globalData.doubanBase;
+        //热映
+        var in_theatersUrl = baseUrl + '/v2/movie/in_theaters';
+        //即将
+        var comingSoonUrl = baseUrl + '/v2/movie/coming_soon';
+        //top 250
+        var top250Url = baseUrl + '/v2/movie/top250';
+
+        this.getMovieListData(in_theatersUrl);
+        this.getMovieListData(comingSoonUrl);
+        this.getMovieListData(top250Url);
+    },
+
+    getMovieListData: (url)=> {
+        
+        wx.request({
+            url: url,
+            header: {
+                'Content-Type': 'application/xml'
             },
-            success:(res)=>{
+            data: {
+                'count' : '3'
+            },
+            success: (res) => {
                 console.log(res);
             },
 
-            fail:(error)=>{
+            fail: (error) => {
                 console.log(error);
             },
 
-            complete:()=>{
+            complete: () => {
 
             },
         })
-    }
+    },
 })
