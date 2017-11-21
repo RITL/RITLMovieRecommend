292d4fd905fcf1deee7dfaa85a827c5e9bae20d8
774655048@qq.com <774655048@qq.com>
2017-11-21 15:10:59 +0800

    movies page is complete

--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -4,6 +4,12 @@ Page({
     // RESTFul API JSON ****需了解****
     // SOAP XML WSDL
 
+    data:{
+        inTheaters:{},
+        comingSoon:{},
+        top250:{}
+    },
+
     onLoad: function(option) {
 
         // 基础的url
@@ -15,23 +21,27 @@ Page({
         //top 250
         var top250Url = baseUrl + '/v2/movie/top250';
 
-        this.getMovieListData(in_theatersUrl);
-        this.getMovieListData(comingSoonUrl);
-        this.getMovieListData(top250Url);
+        this.getMovieListData(in_theatersUrl,'inTheaters');
+        this.getMovieListData(comingSoonUrl,'comingSoon');
+        this.getMovieListData(top250Url,'top250');
     },
 
-    getMovieListData: (url)=> {
-        
+    getMovieListData: function (url, key) {
+
+        var that = this;
         wx.request({
             url: url,
             header: {
                 'Content-Type': 'application/xml'
             },
+
             data: {
                 'count' : '3'
             },
-            success: (res) => {
+
+            success: function(res) {
                 console.log(res);
+                that.processDoubanData(res.data,key);
             },
 
             fail: (error) => {
@@ -42,5 +52,34 @@ Page({
 
             },
         })
+    },
+
+    processDoubanData: function(moviesDouban ,key) {
+            var movies = [];
+
+            // 遍历 对标题进行出处理
+            for(var idx in moviesDouban.subjects){
+
+                var subject = moviesDouban.subjects[idx];
+                var title = subject.title;
+                if(title.length >= 6){
+                    title = title.substring(0,6) + "...";
+                }
+
+                var temp = {
+                    title : title,
+                    average : subject.rating.average,
+                    coverageUrl:subject.images.large,
+                    movieId:subject.id
+                }
+
+                movies.push(temp);
+            }
+
+            var readyData = {};
+            readyData[key] = {
+                movies: movies,
+            };
+            this.setData(readyData);
     },
 })
